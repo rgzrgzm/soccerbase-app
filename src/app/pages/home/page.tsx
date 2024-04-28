9efbeb0079704dd8c5678b7485d0f39ec3cbc17b
rgzrgzm <mariomkt97@gmail.com>
2024-04-27 21:19:53 -0700

    adding styles

--- src/app/pages/home/page.tsx
+++ src/app/pages/home/page.tsx
@@ -2,7 +2,7 @@ import { MatchSchedule } from "@/app/components";
 
 const HomePage = () => {
   return (
-    <div className="h-screen pt-[80px] font-black text-[19px]">
+    <div className="h-screen pt-[80px] bg-[#e5e7eb] font-black text-[19px]">
       <h1>¡Bienvenido a la página de inicio!</h1>
 
       <MatchSchedule />
