a788948cc912fca0a91866cf1a6b23ce7739742d
rgzrgzm <mariomkt97@gmail.com>
2024-04-27 20:30:14 -0700

    first commit to deploy

---
+++ src/app/home/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      <h1>¡Bienvenido a la página de inicio!</h1>
+      {/* Aquí puedes agregar más contenido */}
+    </div>
+  );
+};
+
+export default HomePage;
