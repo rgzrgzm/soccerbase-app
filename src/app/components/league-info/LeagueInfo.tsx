ec38ea9917f7286381b256e6f33905305d1e8379
rgzrgzm <mariomkt97@gmail.com>
2024-04-30 22:30:55 -0700

    added cleaning path logo

--- src/app/components/league-info/LeagueInfo.tsx
+++ src/app/components/league-info/LeagueInfo.tsx
@@ -8,7 +8,7 @@ interface Props {
 export const LeagueInfo = ({ leagueName, leagueLogo }: Props) => {
   return (
     <div className="flex items-center gap-2">
-      <Image width={25} height={25} src={leagueLogo.toString()} alt={leagueLogo} />
+      <Image width={25} height={25} src={leagueLogo.trim()} alt={leagueLogo} />
       <span className="text-[17px]">{leagueName}</span>
     </div>
   );
